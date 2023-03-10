2f5aeea1309d9e2a15c834041c24d56d73d8d731
rudi <rudidankk@gmail.com>
2023-03-10 21:40:12 +0000

    Completed ArraySum Lab

--- src/main/java/ArraySum.java
+++ src/main/java/ArraySum.java
@@ -7,7 +7,13 @@ public class ArraySum {
      * @return the sum of all values in arr.
      */
     public int sum(int[] arr){
+        
+        int sum = 0;
+        int i;
+        for (i = 0; i < arr.length; i++)
+        sum = sum + arr[i];
 
-        return 0;
+        return sum; 
     }
 }
+
